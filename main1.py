937d64ca81dbf54db2369f47647d2bc702adbb4b
OlegDementev <olegdementev2012@gmail.com>
2025-07-16 15:21:25 +0400

    commit_4

--- main1.py
+++ main1.py
@@ -1,16 +1,59 @@
-s = "orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley"
+# f = open("C:\\Users\\usertolko\\Documents\\work_folder\\learning_python\\txt.txt")
 
-s_ = s.split()
+# txt = f.readlines()
 
-res = {}
+# txt.pop(0)
 
-for i in s_:
-    res[i] = res.get(i, 0) + 1
+# coord_x = []
+# coord_y = []
 
-max_key = max(res.values())
+# for i in txt:
+#     elem = i.split()
+#     coord_x.append(elem[0])
+#     coord_y.append(elem[1][:-2])
 
-n_res = {i: j for i, j in res.items() if j == max_key}
+# for i in range(len(coord_x)):
+#     coord_x[i] = coord_x[i].replace(",", ".")
+#     coord_y[i] = coord_y[i].replace(",", ".")
 
-n_res__ = min(n_res)
+# for i in range(len(coord_x)):
+#     coord_x[i] = float(coord_x[i])
+#     coord_y[i] = float(coord_y[i])
 
-print(n_res__)
+# coord_xy = dict(zip(coord_x, coord_y))
+
+# line = 10
+
+
+# cluster_0 = []
+# cluster_1 = []
+
+# for i in coord_xy.items():
+#     if i[1] >= 10:
+#         cluster_0.append(i)
+#     else:
+#         cluster_1.append(i)
+
+
+# res_center = []
+
+# # min_max = (
+# #     (cluster_0[i + 1][0] - cluster_0[i][0]) ** 2
+# #     + (cluster_0[i + 1][1] - cluster_0[i][1]) ** 2
+# # ) ** 0.5
+
+# minimum = 10**10
+# for i in range(len(cluster_0) - 1):
+#     min_sum = []
+#     for _ in range(len(cluster_0) - 1):
+#         x1 = cluster_0[i][0]
+#         y1 = cluster_0[i][1]
+#         x2 = cluster_0[i + 1][0]
+#         y2 = cluster_0[i + 1][1]
+#         d_a_b = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+#         min_sum.append(d_a_b)
+#     summm = sum(min_sum)
+#     minimum = min(minimum, summm)
+
+
+# print(minimum)
